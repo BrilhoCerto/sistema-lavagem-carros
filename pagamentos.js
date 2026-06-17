95d92525bf0e45bbc1a49782b12a46685b518bbe
BrilhoCerto <brilhocertolavagemdecarros@gmail.com>
2026-06-17 16:54:46 +0100

    Update pagamentos.js

--- pagamentos.js
+++ pagamentos.js
@@ -560,7 +560,7 @@ if(btnFiltrar){
 
         const dataInicial =
         document.getElementById("filtroDataInicial").value;
-        alert(dataInicial);
+        alert(FILTRO NOVO);
 
         const dataFinal =
         document.getElementById("filtroDataFinal").value;
